078e2cc19171009ed3ceb48df20555d31718367a
kyanrong <kekyanrong@gmail.com>
2014-04-11 16:33:58 +0800

    Added more resources, added zoomSlider and searchLineEdit (TBC)

--- client/bustrackwindow.cpp
+++ client/bustrackwindow.cpp
@@ -6,14 +6,45 @@ BusTrackWindow::BusTrackWindow(QWidget *parent) :
     ui(new Ui::BusTrackWindow)
 {
     ui->setupUi(this);
-    mapPixmap = QPixmap(":/resources/map.png");
-    mapScene.addPixmap(mapPixmap);
-    ui->mapView->setScene(&mapScene);
+    setMap();
 }
 
 BusTrackWindow::~BusTrackWindow()
 {
     //testing commit //terence
     delete ui;
+}
+
+// event handlers
+
+// override default mouse behaviour to implement zoom
+void BusTrackWindow::wheelEvent(QWheelEvent *event)
+{
+
+}
+
+void BusTrackWindow::resizeEvent(QResizeEvent *event)
+{
+
+}
+
+
+void BusTrackWindow::setMap()
+{
+    mapPixmap = QPixmap(":/resources/map.png");
+    mapScene.addPixmap(mapPixmap);
+    ui->mapView->setScene(&mapScene);
+}
+
+void BusTrackWindow::scaleMap(float zoom)
+{
+    if(zoom < 0)
+    {
+        return;
+    }
+    else
+    {
+         ui->mapView->scale(zoom, zoom);
+    }
 }
 
